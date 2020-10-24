42bc61c7ec2ca2c043b03cc45fdbae037ad47094
Steve Orchosky <sorchosky@gmail.com>
2020-10-24 19:49:12 -0400

    converted to radio buttons

---
+++ homework_6/scripts.js
@@ -0,0 +1,14 @@
+// ADD BUN TO AN ARRAY
+// set up array
+// create a bun object (class?)
+// onclick function for bun
+// create a new bun object
+// get bun flavor
+// get icing flavor
+// get amount
+// get subtotal
+// store the new array to local storage
+
+// DISPLAY BUN ARRAY TO BASKET PAGE
+// get basket list
+// loop through each bun in bun array
