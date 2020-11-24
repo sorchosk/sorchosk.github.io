e088b34979dbdb1ad6ee87a2502757b0daa0b34e
Steve Orchosky <sorchosky@gmail.com>
2020-11-24 17:48:54 -0500

    Resume javascript loop

--- homework_8/build/scripts.js
+++ homework_8/build/scripts.js
@@ -1,3 +1,80 @@
+'use strict';
+
+// PROJECT ARRAY
+const jobList = [
+    {
+        img: 'img/professional-experience/icon.jpeg',
+        title: 'Digital Designer',
+        company: 'Icon Marketing',
+        dates: 'APR 2017–AUG 2020',
+        description: [
+            'Designed and developed websites for The U.S. Playing Card, Company, Union Savings Bank, Guardian Savings Bank, Raynor Gaming, and Braxton Brewery',
+            'Concepted and implemented a landing page build system to deliver over 200 custom landing pages for retailers of Tempur-Pedic, Sealy, and Stearns & Foster',
+            'Managed the digital design internship program'
+        ]
+    },
+    {
+        img: 'img/professional-experience/rockfish.png',
+        title: 'Freelance Designer',
+        company: 'Rockfish',
+        dates: 'NOV 2016–JAN 2017',
+        description: [
+            'Create Amazon marketplace and e-commerce graphics for MARS Petcare',
+            'Exhibit organization skills for thousands of graphics and appropriately package them for handoff to project management and clients',
+            'Illustrate infographics for use in MARS Petcare marketing materials while adhering to their brand guidelines'
+        ]
+    },
+]
+
+'use strict';
+
+let workExperience = document.getElementById('workExperience');
+
+let workLoop = () => {
+    for (let job in jobList) {
+        let title = jobList[job].title;
+        let company = jobList[job].company;
+        let dates = jobList[job].dates;
+        let img = jobList[job].img;
+
+        // Render work experience meta information
+        let workExperienceItem = document.createElement('div');
+        workExperienceItem.classList = 'row work-experience-item';
+
+        workExperienceItem.innerHTML = `
+        <div class="col col-12 col-lg-4">
+            <div class="row no-gutters work-experience-meta">
+                <img src="` + img + `" alt="` + company + ` logo" class="img-fluid work-experience__img">
+                <div class="col">
+                    <h4>` + title + `</h4>
+                    <h5>` + company + `</h5>
+                    <h6>` + dates + `</h6>
+                </div>
+            </div>
+        </div>
+        `
+
+        // Render description bullets
+        let description = jobList[job].description;
+        let itemizedDescriptionDiv = document.createElement('div');
+        itemizedDescriptionDiv.classList = "col col-12 col-lg-8";
+        let itemizedDescription = document.createElement('ul');
+        itemizedDescription.classList = 'work-experience__ul';
+        let listItem;
+        for (let bullet in description) {
+            let listItem = document.createElement('li');
+            listItem.classList = 'work-experience__li';
+            listItem.innerHTML = description[bullet];
+            itemizedDescription.appendChild(listItem);
+        }
+        itemizedDescriptionDiv.appendChild(itemizedDescription);
+
+        
+        workExperience.appendChild(workExperienceItem);
+        workExperienceItem.appendChild(itemizedDescriptionDiv);
+    }
+}
+
 let navToggleIcon = document.getElementById("navToggleIcon");
 
 // 
---
+++ homework_8/src/js/jobListArray.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// PROJECT ARRAY
+const jobList = [
+    {
+        img: 'img/professional-experience/icon.jpeg',
+        title: 'Digital Designer',
+        company: 'Icon Marketing',
+        dates: 'APR 2017–AUG 2020',
+        description: [
+            'Designed and developed websites for The U.S. Playing Card, Company, Union Savings Bank, Guardian Savings Bank, Raynor Gaming, and Braxton Brewery',
+            'Concepted and implemented a landing page build system to deliver over 200 custom landing pages for retailers of Tempur-Pedic, Sealy, and Stearns & Foster',
+            'Managed the digital design internship program'
+        ]
+    },
+    {
+        img: 'img/professional-experience/rockfish.png',
+        title: 'Freelance Designer',
+        company: 'Rockfish',
+        dates: 'NOV 2016–JAN 2017',
+        description: [
+            'Create Amazon marketplace and e-commerce graphics for MARS Petcare',
+            'Exhibit organization skills for thousands of graphics and appropriately package them for handoff to project management and clients',
+            'Illustrate infographics for use in MARS Petcare marketing materials while adhering to their brand guidelines'
+        ]
+    },
+]
