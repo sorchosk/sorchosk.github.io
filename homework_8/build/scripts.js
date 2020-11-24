16592b67aa3f6585af53789d3ee7c0a00a525f6a
Steve Orchosky <sorchosky@gmail.com>
2020-11-24 16:32:40 -0500

    work experience display

--- homework_8/build/scripts.js
+++ homework_8/build/scripts.js
@@ -1,3 +1,20 @@
+let navToggleIcon = document.getElementById("navToggleIcon");
+
+// 
+function nav() {
+    
+}
+
+// OPEN NAV
+function openNav() {
+    alert("Nav opened");
+}
+
+// CLOSE NAV
+function closeNav() {
+    alert("Nav closed");
+}
+
 'use strict';
 
 // PROJECT ARRAY
---
+++ homework_8/src/js/openNav.js
@@ -0,0 +1,16 @@
+let navToggleIcon = document.getElementById("navToggleIcon");
+
+// 
+function nav() {
+    
+}
+
+// OPEN NAV
+function openNav() {
+    alert("Nav opened");
+}
+
+// CLOSE NAV
+function closeNav() {
+    alert("Nav closed");
+}
